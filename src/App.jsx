b6bca7654b8d54db32b295fe63129d1fc6606f6e
Jonathan van Oudheusden <jonathanvanoudheusden@gmail.com>
2025-08-28 12:58:12 +0000

    select and return from email

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import { useState } from 'react'
 import initialEmails from './data/emails'
 
 import './styles/App.css'
-import emailList from './components/EmailList'
+// import EmailList from './components/EmailList'
 import Header from './components/Header'
 import LeftMenu from './components/LeftMenu'
+import ShowEmails from './components/ShowEmails'
 // import email from './components/email'
 
 const getReadEmails = emails => emails.filter(email => !email.read)
@@ -16,6 +17,8 @@ function App() {
   const [emails, setEmails] = useState(initialEmails)
   const [hideRead, setHideRead] = useState(false)
   const [currentTab, setCurrentTab] = useState('inbox')
+
+  const [selectedEmail, setSelectedEmail ] = useState(null);
 
   const unreadEmails = emails.filter(email => !email.read)
   const starredEmails = emails.filter(email => email.starred)
@@ -34,11 +37,12 @@ function App() {
       {Header()}
 
       {LeftMenu(currentTab, setCurrentTab, unreadEmails, starredEmails, hideRead, setHideRead)}
-      {emailList(filteredEmails, setEmails)}
+      {ShowEmails(selectedEmail, setSelectedEmail, filteredEmails, setEmails)}
     </div>
   )
 }
 
+// {/* {EmailList(filteredEmails, setEmails)} */}
 export default App;
 
 
